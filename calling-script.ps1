9e02f0bc589257746156a0e8cb1f9f4a44da79ef
Brett Barker <brett.barker@brbtechsolutions.com>
2023-09-03 00:38:55 +0000

    add test call for format-softwarelist

--- calling-script.ps1
+++ calling-script.ps1
@@ -1,5 +1,11 @@
 # Load the Compare-CSV.ps1 script
 . .\Compare-CSV.ps1
 
+# Load the Format-SoftwareList.ps1 script
+. .\Format-SoftwareList2.ps1
+
 # Call the Compare-CSV function
 Compare-CSV -Csv1Path "./test-data/file1.csv" -Csv2Path "./test-data/file2.csv" -ComparisonColumn "name"
+
+# Call the Format-SoftwareList function
+Format-SoftwareList -SoftwareListPath "./test-data/software1.csv"
