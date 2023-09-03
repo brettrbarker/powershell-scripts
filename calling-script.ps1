8a8c795adc606244f15116ab1b5d7362ef994fe9
Brett Barker <brett.barker@brbtechsolutions.com>
2023-09-03 13:52:33 -0500

    testing different call options

--- calling-script.ps1
+++ calling-script.ps1
@@ -5,7 +5,8 @@
 . .\Format-SoftwareList.ps1
 
 # Call the Compare-CSV function
-Compare-CSV -Csv1Path "test-data\file1.csv" -Csv2Path "test-data\file2.csv" -ComparisonColumn "name"
+#Compare-CSV -Csv1Path "test-data\file1.csv" -Csv2Path "test-data\file2.csv" -ComparisonColumn "name"
+#Compare-CSV
 
 # Call the Format-SoftwareList function
 #Format-SoftwareList -SoftwareListPath "./test-data/software1.csv"
