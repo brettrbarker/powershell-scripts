160ef6cfeb6d41c3ae69cdae422ff7cdf0fe99b3
Brett Barker <brett.barker@brbtechsolutions.com>
2023-09-03 00:42:18 +0000

    promote version of format-softwarelist

--- Format-SoftwareList.ps1
+++ Format-SoftwareList.ps1
@@ -39,18 +39,57 @@ function Format-SoftwareList {
     # If the 'SoftwareTemp' column contains a "|", remove it and everything following it from the 'Software' column. And remove any trailing whitespace.
     $csv = $csv | Select-Object *,@{Name='Software';Expression={($_.SoftwareTemp -split '\|')[0] -replace '\s+$',''}} | Select-Object -Property "Software","Version","Count"
 
-    # OS Column
-    #$csv = $csv | Select-Object *,@{Name='OS';Expression={if($_.Software -like '*Windows 10*') {'Windows'} else {$_.OS}}}
-
     # Add a new column to the CSV called "OS" that is initially blank for all rows
-    #$csv = $csv | Select-Object *,@{Name='OS';Expression={""}} | Select-Object -Property "Software","Version","OS","Count"
-    # If the 'Software' column contains "Windows", add "Windows" to the 'OS' column
-    #$csv = $csv | Select-Object *,@{Name='OS';Expression={if($_.Software -like "*Windows*") {"Windows"}}} | Select-Object -Property "Software","Version","OS","Count"
-    # If the 'Software' column contains "Linux" or ".el7" or ".el8", add "Linux" to the 'OS' column
-    #$csv = $csv | Select-Object *,@{Name='OS';Expression={if($_.Software -like "*Linux*"){"Linux"}}} | Select-Object -Property "Software","Version","OS","Count"
-    #$csv = $csv | Select-Object *,@{Name='OS';Expression={if($_.Software -like "*.el7*"){"RHEL 7"}}} | Select-Object -Property "Software","Version","OS","Count"
-    #$csv = $csv | Select-Object *,if($_.Software -like "*.el8*")@{Name='OS';Expression={"RHEL 8"}}else@{Name='OS';Expression={"cheese"}} | Select-Object -Property "Software","Version","OS","Count"
-    
+    $csv = $csv | Select-Object *,@{Name='OS';Expression={""}} | Select-Object -Property "Software","Version","OS","Count"
+      
+    # Loop through each object in the CSV and set the OS column based on the Software column
+    $csv = $csv | ForEach-Object {
+        if($_.Software -like "*Windows*" -or $_.Software -like "*Microsoft*") {
+            $_.OS = "Windows"
+        }
+        elseif($_.Software -like "*el7*") {
+            $_.OS = "RHEL7"
+        }
+        elseif($_.Software -like "*el8*") {
+            $_.OS = "RHEL8"
+        }
+        else {
+            $_.OS = ""
+        }
+        # if Software has a "-" at the end of it, remove it and trailing whitespace.
+        $_.Software = $_.Software -replace '-$','' -replace '\s+$',''
+        $_
+    }
+
+    # Find rows with matching 'Software'
+    $csv = $csv | Group-Object -Property @("Software", "Version") | ForEach-Object {
+        # If there is more than one row with the same 'Software' and 'Version' values, combine them into one row
+        if($_.Count -gt 1) {
+            # Combine the 'Version' column values into a single string
+            $Version = ($_.Group | Select-Object -ExpandProperty Version) -join ","
+            # Set 'Version' to the same value for all rows
+            $Version = $Version.Split(",")[0]
+            # Combine the 'OS' column values into a single string
+            $OS = ($_.Group | Select-Object -ExpandProperty OS) -join ","
+            # Set 'OS' to the same value for all rows
+            $OS = $OS.Split(",")[0]
+            # Convert the 'Count' column values into integers and add them together
+            $Count = ($_.Group | Select-Object -ExpandProperty Count | ForEach-Object {[int]$_}) | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+            
+            
+            # Create a new object with the combined values
+            [PSCustomObject]@{
+                Software = $_.Name.Split(",")[0]
+                Version = $Version
+                OS = $OS
+                Count = $Count
+            }
+        }
+        # If there is only one row with the same 'Software' value, return the row as-is
+        else {
+            $_.Group
+        }
+    }
 
     # Export the output CSV file
     # Check if $OutputPath is null, if so, use the default path
