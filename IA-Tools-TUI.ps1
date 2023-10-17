7bffc911a695b26d48d999b6b8ec0096f03bfe56
Brett Barker <brett.barker@brbtechsolutions.com>
2023-10-17 14:25:37 -0500

    revise menu

--- IA-Tools-TUI.ps1
+++ IA-Tools-TUI.ps1
@@ -1,6 +1,3 @@
-# Load the Compare-CSV.ps1 script
-. .\Compare-CSV.ps1
-
 # Load the Format-SoftwareList.ps1 script
 . .\Format-SoftwareList.ps1
 
@@ -9,31 +6,44 @@
 
 
 
-## LOAD MAIN MENU
-while ($true) {
-    # Print Menu
+function Show-Header {
     Write-Host "##### IA TOOLS #####" -ForegroundColor Green
-    Write-Host "1. Export Active Directory Data"
-    Write-Host "2. Format Software List"
-    Write-Host "3. "
-    Write-Host "..."
-    Write-Host "8. Run Comparison"
-    Write-Host "9. Change Export Location"
-    Write-Host "0. Exit"
-
-    # Prompt user to select 1, 2, or 0 for Export Regular users, Privileged users, or Exit.
-    $selection = Read-Host "Selection"
-
-    # If user selects 1, execute Export-Users.ps1
-    if ($selection -eq 1) {
-        Export-ADData
-    }
-    elseif ($selection -eq 2) {
-        Enter-SoftwareMenu
-    }
-
-    elseif ($selection -eq 0) {
-        break
-    }
-
+    Write-Host "##### Working Directory: " -NoNewline -ForegroundColor Green
+    Write-Host "$exportLocation" -ForegroundColor Red   
 }
+function Enter-MainMenu {
+    while ($true) {
+        # Print Menu
+        Show-Header   
+        Write-Host "1. Active Directory Tools"
+        Write-Host "2. Software List Tools"
+        Write-Host "..."
+        Write-Host "9. Change Working Directory"
+        Write-Host "0. Exit"
+
+        # Prompt user to select 1, 2, or 0 for Export Regular users, Privileged users, or Exit.
+        $selection = Read-Host "Selection"
+
+        # If user selects 1, execute Export-Users.ps1
+        if ($selection -eq 1) {
+            Export-ADData
+        }
+        elseif ($selection -eq 2) {
+            Enter-SoftwareMenu
+        }
+        elseif ($selection -eq 3) {
+            # Enter-SoftwareMenu
+        }
+        elseif ($selection -eq 8) {
+            # Compare-CSV
+        }
+        elseif ($selection -eq 9) {
+            Edit-WorkingDirectory
+        }
+        elseif ($selection -eq 0) {
+            break
+        }
+    }
+}
+
+Enter-MainMenu
