862985b6c81c30fa8c621a9063119b46caa4612e
Brett Barker <57292879+brettrbarker@users.noreply.github.com>
2023-12-18 06:56:35 -0600

    add machine name and serial

--- get-drives.ps1
+++ get-drives.ps1
@@ -1,7 +1,17 @@
+# Get the machine name
+$machineName = (Get-WmiObject -Class Win32_ComputerSystem).Name
+
+Write-Output "Machine Name: $machineName"
+
+# Get the machine serial number
+$machineSerialNumber = (Get-WmiObject -Class Win32_BIOS).SerialNumber
+
+Write-Output "Machine Serial Number: $machineSerialNumber"
+
 # Get all physical drives
 $drives = Get-WmiObject -Class Win32_DiskDrive
 
-# Loop through each drive and print its serial number
+# Loop through each drive and print its information
 foreach ($drive in $drives) {
-    Write-Output "Drive $($drive.DeviceID): $($drive.SerialNumber.Trim())"
+    Write-Output "Drive SerialNumber: $($drive.SerialNumber.Trim())"
 }
